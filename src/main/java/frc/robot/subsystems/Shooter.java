f9c319bb33ec357cbe1736dc0cb3442aef4099dc
Ahmed Abdel-Dayem <97477649+Ahmed4069@users.noreply.github.com>
2024-02-05 14:56:52 -0500

    Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,47 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.ctre.phoenix6.configs.Slot0Configs;
 
-public class Shooter{
-     
+import com.ctre.phoenix6.controls.VelocityVoltage;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+
+public class Shooter {
+    TalonFX talon1;
+    TalonFX talon2;
+    SimpleMotorFeedforward feedforward;
+    final VelocityVoltage v = new VelocityVoltage(0);
+    Slot0Configs config = new Slot0Configs();
+
+    public Shooter(){
+        talon1 = new TalonFX(9);
+        talon2 = new TalonFX(8);
+
+        talon1.setInverted(true);
+
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kV = 0.11811;
+        slot0Configs.kP = 0.11;
+        slot0Configs.kI = 0.48;
+        slot0Configs.kD = 0.01;
+
+        talon1.getConfigurator().apply(slot0Configs, 0.050);
+        talon2.getConfigurator().apply(slot0Configs, 0.050);
+
+
+        //feedforward = new SimpleMotorFeedforward(0.79972, );
+    }
+
+    public void drive(){
+        v.Slot = 0;
+        talon1.setControl(v.withVelocity(80));
+        talon2.setControl(v.withVelocity(40));
+    }
+    public void stop(){
+        talon1.set(0);
+        talon2.set(0);
+    }
 }
--- src/main/java/frc/robot/subsystems/ShooterTest.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
-public class ShooterTest extends SubsystemBase {
-     
-}
