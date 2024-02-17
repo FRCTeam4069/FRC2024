2a68511ef56d7619bab1a0494e1546602b62788a
Petelax <peterxiong101@gmail.com>
2024-02-17 16:54:58 -0500

    added swerve subsystem, waiting for drivebase to add jsons

---
+++ src/main/java/frc/robot/constants/AutonConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants;
+
+import com.pathplanner.lib.util.PIDConstants;
+
+public class AutonConstants {
+    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0, 0, 0);
+    public static final PIDConstants ANGLE_PID = new PIDConstants(0, 0, 0);
+}
