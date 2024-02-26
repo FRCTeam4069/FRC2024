62b6cfbf9e642c5fbf6dae9bb236a4618aca32c2
John Liao <johnliao5@gmail.com>
2024-02-25 23:25:38 -0500

    added sysID to climber system. NOT tested and not added into Main.java yet.

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,20 +1,43 @@
+/*
+ * https://docs.wpilib.org/en/stable/docs/software/advanced-controls/system-identification/introduction.html
+ * https://docs.wpilib.org/en/stable/docs/software/advanced-controls/system-identification/creating-routine.html
+ * 
+ * look at this if having problem with logger:
+ * https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/sysid/subsystems/Shooter.java
+ */
+
+
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import static edu.wpi.first.units.Units.Volts;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.DeviceIDs;
 
 public class ClimberSubsystem extends SubsystemBase {
     CANSparkMax climber;
+
+    SysIdRoutine routine;
 
     // need to tune these !!!
     private static double pK = 0.1;
     private static double pG = 0.1;
 
     public ClimberSubsystem() {
-        //climber = new CANSparkMax(DeviceIDs.CLIMBER, MotorType.kBrushless);
+        climber = new CANSparkMax(DeviceIDs.CLIMBER, MotorType.kBrushless);
+
+        routine = new SysIdRoutine(
+            new SysIdRoutine.Config(), 
+            new SysIdRoutine.Mechanism(this::setClimber, this::logMotor, this));
     }
 
     public void raiseClimber() {
@@ -31,4 +54,22 @@ public class ClimberSubsystem extends SubsystemBase {
         // may need to change this later
         climber.set(-pG + -pK * positionDifference());
     }
+
+    public void setClimber(Measure<Voltage> x) {
+        climber.setVoltage(x.in(Volts));
+    }
+
+    public void logMotor(SysIdRoutineLog x) {
+        
+    }
+
+    // Methods for SysId: BE CAREFUL WHEN USING!!!
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return routine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return routine.dynamic(direction);
+    }
+
 }
